79ffeca083b59d0a570c50b296c2a94c65562b9b
Marius Kleiner <kleiner@gmail.com>
2020-01-04 13:25:02 +0100

    Added basic logging

--- pysubtitles/check.py
+++ pysubtitles/check.py
@@ -2,13 +2,21 @@
 
 import sys
 import getopt
-
-# Global options with default values.
-verbose = False
+import logging
 
 
 def main():
     args = parse_options(sys.argv[1:])
+
+    # Set default logging level (this has no effect if logging.basicConfig has
+    # already been called as part of option parsing).
+    logging.basicConfig(level=logging.INFO)
+
+    logging.debug('This is a debug message')
+    logging.info('This is an info message')
+    logging.warning('This is a warning message')
+    logging.error('This is an error message')
+    logging.critical('This is a critical message')
 
     if len(args) != 1:
         error("Invalid syntax")
@@ -26,19 +34,17 @@ def usage():
 
     Options:
     -h              Show this help text.
+    -q              Quiet mode (show only warnings and errors)
     -v              Display verbose output.
     """)
 
 
 def parse_options(args):
 
-    # Access global variables
-    global verbose
-
     # Parse options using Getopt; display an error and exit if options could not
     # be parsed.
     try:
-        opts, args = getopt.getopt(args, "o:hv")
+        opts, args = getopt.getopt(args, "o:hvq")
     except getopt.GetoptError as err:
         print(str(err))
         usage()
@@ -50,19 +56,17 @@ def parse_options(args):
             usage()
             sys.exit()
         elif opt == "-v":
-            verbose = True
+            logging.basicConfig(level=logging.DEBUG)
+        elif opt == "-q":
+            logging.basicConfig(level=logging.WARN)
         else:
             assert False, "unhandled option"
 
     return args
 
 
-def debug(s):
-    if verbose:
-        sys.stderr.write("DEBUG: " + s + "\n")
-
 def error(s):
-    sys.stderr.write("ERROR: " + s + "\n")
+    sys.stderr.write(s + "\n")
 
 
 # This is the most important line: it calls the main function if this program is
